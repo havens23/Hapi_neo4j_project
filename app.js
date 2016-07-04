ad70c178eb520463972e54296736a36264afc93f
havens23 <havens7839@gmail.com>
2016-07-04 14:53:39 +0900

    job scheduleling update

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const Hapi = require('hapi');
+
+// webhook 연동 테스트 모듈
+const schedule = require('node-schedule');
+var request = require('request');
+
 // Path, Inert, server는 파일 업로드 테스트가 끝나면 삭제!!
 const Path = require('path');
 const Inert = require('inert');
@@ -16,6 +21,53 @@ server.connection({
     host: '192.168.0.11',
     port: 3000
 });
+
+//job scheduleling test
+
+var rule1 = new schedule.RecurrenceRule();
+
+rule1.minute = 51;
+
+var scheduledJob2 = schedule.scheduleJob(rule1,
+    function(){
+
+        console.log('[2] 서버 정상 작동 중!!');
+
+        var formData = {
+            body: '[[PizzaHouse]](http://url_to_text) You have a new Pizza order.', //Body text (Required)
+            connectColor: '#FAC11B', //Hex code color of attachment bar
+            connectInfo: [{
+                title: 'Topping', //1st attachment area title
+                description: 'Pepperoni' //1st attachment description
+            },
+                {
+                    title: 'Location', //2nd attachment area title
+                    description: 'Empire State Building, 5th Ave, New York', //2nd attachment description
+                    imageUrl: 'http://url_to_text' //Image URL
+                }]
+        }
+
+        var options = {
+            url: 'https://wh.jandi.com/connect-api/webhook/12210821/1b7cb6ccb9975f860c744166c3557990',
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/vnd.tosslab.jandi-v2+json"
+            },
+            form: formData
+        };
+        request.post(options, function (err, response, body) {
+            if (err) {
+                console.error('err: ', err);
+                return;
+            }
+
+            console.log('body: ', body);
+            return;
+
+
+        });
+    }
+);
 
 //Route configuration
 server.register({
